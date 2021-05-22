a7fb20e3bead74f0a378319103aac77a57a1c01d
Carolina Tamanho Barbosa <83873248+lolabarbosa@users.noreply.github.com>
2021-05-22 11:11:09 -0300

    update

--- exercicio4.c
+++ exercicio4.c
@@ -2,7 +2,7 @@
 //4º problema: desenvolva um programa que leia 3 notas de 40 alunos, calcule e mostre a média aritmética e a situação de aprovação de cada um deles. Lembre-se que apenas a média igual ou acima de 7 aprova o aluno.
 int main(){
     int alunos, a=1, provas, b=1;
-    float nota1, nota2, nota3, soma=0, media, nota;
+    float soma=0, media, nota;
     printf("Digite o numero de alunos: \n");
     scanf("%d", &alunos); //numero de alunos
     printf("Digite o numero de provas: \n");
@@ -16,13 +16,12 @@ int main(){
         media=soma/provas;
         if (media>=7){
             printf("O aluno esta aprovado com a media de %.2f! \n", media);
-
         } else if (media>=5 || media<7){
             printf("O aluno esta em recuperacao com a media de %.2f! \n", media);
-
         } else {
             printf("O aluno esta reprovado com a media de %.2f! \n", media);
-
         }
+        media=0;
+        soma=0;
         }
     }
